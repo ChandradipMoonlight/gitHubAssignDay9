3714a5156963afaee7240c5c0db598986e2831ae
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-07 17:28:15 +0530

    step 3 daily wage of emp calculated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,11 +2,19 @@
 
 echo Welcome to Employee Wage Coputation Program on Manin Branch.
 
-checkEmp=$((RANDON%2))
+checkEmp=$((RANDOM%2))
+
+empRate=20
 
 case $checkEmp in
 	1)
+		empHr=8
 		echo "Employee is Present" ;;
 	0)
+		empHr=0
 		echo "Employee is Absent" ;;
 esac
+
+empDailyWage=$(($empRate*$empHr))
+
+echo $empDailyWage
