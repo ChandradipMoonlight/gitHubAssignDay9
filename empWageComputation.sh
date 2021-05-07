10bcd27a33cdc8fa12657c517101374010a7edaf
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-07 17:08:14 +0530

    second step of empWage problem done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #! /bin/bash -x
 
 echo Welcome to Employee Wage Coputation Program on Manin Branch.
+
+checkEmp=$((RANDON%2))
+
+case $checkEmp in
+	1)
+		echo "Employee is Present" ;;
+	0)
+		echo "Employee is Absent" ;;
+esac
